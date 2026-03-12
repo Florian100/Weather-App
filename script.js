2580c3040cc0407d6e8e9400d03a3699263f03c4
Florian100 <fxheka@uet.edu.al>
2026-03-12 23:25:21 +0000

    Remove commented code related to weather icon handling in script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const weatherInfo = document.getElementById('weather-info');
 const cityName = document.getElementById('city-name');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
-// Variabli i ri për ikonën
 const weatherIcon = document.getElementById('weather-icon');
 
 searchBtn.addEventListener('click', () => {
@@ -40,7 +39,6 @@ async function getWeather(city) {
         temperature.textContent = `${Math.round(data.main.temp)}°C`;
         description.textContent = data.weather[0].description;
         
-        // Kodi që merr ikonën dhe e kthen në një imazh të dukshëm
         const iconCode = data.weather[0].icon;
         weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
         
